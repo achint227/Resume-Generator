49f92749c62eff5f16955ec82ac83f9e9a4f489d
Achint Bhat <achint94@asu.edu>
2023-02-02 02:04:22 -0700

    working draft

--- build_resume.py
+++ build_resume.py
@@ -28,77 +28,136 @@ def apply_latex_escape(d):
     return d
 
 
-def new_section(section_name, content):
+def new_section(section_name, content, new_template=False):
+    if new_template:
+        if section_name.casefold() == "summary":
+            content = "\n\\begin{cvparagraph}\n" + content + "\n\\end{cvparagraph}"
+        else:
+            content = "\n\\begin{cventries}\n\n" + content + "\n\\end{cventries}"
+
+        return f"\\cvsection{{{section_name}}}\n{content}"
     return f"\\section{{{section_name}}}\n{content}"
 
 
-def generate_resume_content(user):
-    with open("user.json", "r") as f:
+def generate_resume_content(user_json, filter=None, new_template=False):
+    with open(f"{user_json}", "r") as f:
         user = load(f)
 
     user = apply_latex_escape(user)
 
-    education = new_section("Education", load_education(user["education"]))
+    education = new_section(
+        "Education", load_education(user["education"], new_template), new_template
+    )
 
-    experience = new_section("Experience", load_experiences(user["experiences"]))
-    projects = new_section("Projects", load_projects(user["projects"]))
+    experience = new_section(
+        "Experience",
+        load_experiences(user["experiences"], filter, new_template),
+        new_template,
+    )
+    projects = new_section(
+        "Projects",
+        load_projects(user["projects"], filter, new_template),
+        new_template,
+    )
 
     summary = (
-        f"""\\section{{Summary}}
-    {{{user["basic_info"].get("summary")}}}"""
+        new_section("Summary", user["basic_info"].get("summary"), new_template)
         if user["basic_info"].get("summary")
         else ""
     )
     homepage = (
         f"\\homepage[{user['basic_info']['homepage']}]{{http://{user['basic_info']['homepage']}}}"
-        if user["basic_info"].get("homepage")
+        if not new_template and user["basic_info"].get("homepage")
         else ""
     )
-    section1, section2, section3 = (
+    sections = (
+        summary,
         experience,
         projects,
         education,
     )
-    RESUME = f"""
-    % !TEX program = xelatex
-
-    \\documentclass{{resume}}
-
-    \\begin{{document}}
-    \\pagenumbering{{gobble}} % suppress displaying page number
-
-    \\name{{{user["name"]}}}
-
-    \\basicInfo{{
-    \\email{{{user["basic_info"]["email"]}}} 
-    \\phone{{{user["basic_info"]["phone"]}}} 
-    \\linkedin[{user["basic_info"]["linkedin"]}]{{https://www.linkedin.com/in/{user["basic_info"]["linkedin"]}}}
-    \\github[{user["basic_info"]["github"]}]{{https://github.com/{user["basic_info"]["github"]}}}
-    {homepage}
-    }}
-    {summary}
-
-    {section1}
-
-    {section2}
-
-    {section3}
-
-    \\end{{document}}
-
-    """
-    return RESUME
+    if new_template:
+        RESUME = f"""
+\\documentclass[11pt, a4paper]{{russell}}
+\\geometry{{left=1.4cm, top=.8cm, right=1.4cm, bottom=1.8cm, footskip=.5cm}}
+\\fontdir[fonts/]
+\\colorlet{{russell}}{{russell-black}}
 
 
-if __name__ == "__main__":
-    RESUME = generate_resume_content("user.json")
-    filename = "Resume1.tex"
+\\setbool{{acvSectionColorHighlight}}{{true}}
+
+\\renewcommand{{\\acvHeaderSocialSep}}{{\\quad\\textbar\\quad}}
+
+\\name{'{'+'}{'.join([_ for _ in user.get('name').split()])+'}'}
+
+\\address{{{user['basic_info'].get('address')}}}
+
+\\mobile{{{user['basic_info'].get('phone')}}}
+\\email{{{user['basic_info'].get('email')}}}
+\\github{{{user['basic_info'].get('github')}}}
+\\linkedin{{{user['basic_info'].get('linkedin')}}}
+\\begin{{document}}
+
+\\makecvheader
+
+
+{sections[0]}
+
+{sections[1]}
+
+{sections[2]}
+
+{sections[3]}
+
+
+\\vspace*{{\\fill}}
+\\end{{document}}
+
+"""
+    else:
+        RESUME = f"""
+% !TEX program = xelatex
+
+\\documentclass{{resume}}
+
+\\begin{{document}}
+\\pagenumbering{{gobble}} % suppress displaying page number
+
+\\name{{{user["name"]}}}
+
+\\basicInfo{{
+\\email{{{user["basic_info"]["email"]}}} 
+\\phone{{{user["basic_info"]["phone"]}}} 
+\\linkedin[{user["basic_info"]["linkedin"]}]{{https://www.linkedin.com/in/{user["basic_info"]["linkedin"]}}}
+\\github[{user["basic_info"]["github"]}]{{https://github.com/{user["basic_info"]["github"]}}}
+{homepage}
+}}
+{sections[0]}
+
+{sections[1]}
+
+{sections[2]}
+
+{sections[3]}
+
+\\end{{document}}
+
+"""
+    return (RESUME, "template2" if new_template else "template")
+
+
+def main(user_file, filename, filter=None, new_template=False):
+    RESUME, template = generate_resume_content(user_file, filter, new_template)
     with open(filename, "w") as output_tex:
         output_tex.write(RESUME)
-    system(f"mv {filename} template/")
-    chdir("template")
+    system(f"mv {filename} {template}/")
+    chdir(f"{template}")
     system(f"xelatex -synctex=1 -interaction=nonstopmode {filename}")
     filename = filename[:-4]
     system(f"mv {filename}.pdf ..")
     system(f"mv {filename}.tex ..")
     system(f"rm {filename}.*")
+
+
+if __name__ == "__main__":
+    main("user.json")
--- load_sections.py
+++ load_sections.py
@@ -9,23 +9,42 @@ def make_bold(string, bold_words):
     return string
 
 
-def bullets_from_list(items, end=True, highlight=None):
+def new_template_project(project, org="", location="", duration="", items=[]):
+    if type(items) == list:
+        items = bullets_from_list(items, new_template=True)
+    return f"""
+
+\\cventry
+{{{org}}}
+{{{project}}}
+{{{location}}}
+{{{duration}}}
+{{{items}}}"""
+
+
+def bullets_from_list(items, end=True, highlight=None, new_template=False):
     if not items:
         return ""
-    res = "\\begin{itemize}\n"
+    cmd = "cvitems" if new_template else "itemize"
+    res = f"\\begin{{{cmd}}}"
     for item in items:
         text = make_bold(item, highlight)
-        res += f"\\item {text}"
+        res += f"\n\\item {{{text}}}"
     if end:
-        res += "\n\\end{itemize}"
-    return res
+        res += f"\n\\end{{{cmd}}}"
+    return f"\n{res}\n"
 
 
-def create_details(project):
+def create_details(project, filter=None):
     if not project:
         return ""
     rs = "\\begin{itemize}\n"
     for p in project:
+        if filter and (
+            not any([_.lower() in [_.lower() for _ in p.get("tools")] for _ in filter])
+        ):
+            continue
+
         rs += f"""
 \\item \\textbf{{{p['title']}}} {{\\hfill {{{', '.join(p['tools'])}}}}}
 {bullets_from_list(p['details'])}
@@ -33,23 +52,49 @@ def create_details(project):
     return rs + "\n\\end{itemize}"
 
 
-def create_experience(exp):
+def create_experience(exp, filter=None, new_template=False):
+    if new_template:
+        return (
+            new_template_project(
+                exp["company"], exp["title"], exp["location"], exp["duration"]
+            )
+            + "\n\\begin{cvparagraph}\n"
+            + create_details(exp["projects"], filter)
+            + "\n\\end{cvparagraph}\n"
+        )
     rs = f"""
 \\datedsubsection{{\\textbf{{{exp['company']}}}}}{{{exp['location']}}}
 \\role{{{exp['title']}}} {{\\hfill {exp['duration']}}}
-{create_details(exp['projects'])}
+{create_details(exp['projects'],filter)}
 """
     return rs
 
 
-def load_experiences(experience):
+def load_experiences(experience, filter=None, new_template=False):
     rs = ""
     for exp in experience:
-        rs += create_experience(exp)
+        rs += create_experience(exp, filter, new_template)
     return rs
 
 
-def create_project(project):
+def create_project(project, new_template=False):
+    if new_template:
+        repo = (
+            "\\href{"
+            + project.get("repo")
+            + "}{"
+            + project.get("repo").split("/")[-1]
+            + "}"
+            if project.get("repo")
+            else ""
+        )
+        return new_template_project(
+            project.get("title"),
+            ", ".join(project.get("tools")),
+            repo,
+            "",
+            project.get("description"),
+        )
     repo = (
         f"{{Repo: }}\\github[{project['repo'].split('/')[-1]}]{{{project['repo']}}}"
         if project.get("repo")
@@ -66,22 +111,36 @@ def create_project(project):
     return rs
 
 
-def load_projects(projects):
+def load_projects(projects, filter=None, new_template=False):
     rs = ""
     for p in projects:
-        rs += create_project(p)
+        if filter and not bool(p.get("repo")):
+            continue
+
+        if filter and (
+            not any([_.lower() in [_.lower() for _ in p.get("tools")] for _ in filter])
+        ):
+            continue
+        rs += create_project(p, new_template)
     return rs
 
 
-def create_education(education):
-    rs = f"""\\datedsubsection{{\\textbf{{{education["university"]}}}}}{{{education.get("location"," ")}}}
+def create_education(education, new_template=False):
+    if new_template:
+        return new_template_project(
+            education.get("university"),
+            education.get("degree", ""),
+            education.get("location", ""),
+            education.get("duration", ""),
+            education.get("info", []),
+        )
+    return f"""\\datedsubsection{{\\textbf{{{education["university"]}}}}}{{{education.get("location","")}}}
 \\role{{{education["degree"]}}} {{\\hfill {education["duration"]}}}
 """
-    return rs
 
 
-def load_education(education):
+def load_education(education, new_template=False):
     rs = ""
     for ed in education:
-        rs += create_education(ed)
+        rs += create_education(ed, new_template)
     return rs
