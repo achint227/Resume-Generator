d6ce4b8f5fbbe133ba928b115d618a0bfb2f4464
Achint Bhat <achint94@asu.edu>
2023-04-10 18:03:24 -0700

    add function to split string

--- template.py
+++ template.py
@@ -2,6 +2,12 @@ from abc import ABC, abstractmethod
 import re
 
 
+def split_string(s, sep=" "):
+    return '{' + \
+        '}{'.join(
+            [_.strip() for _ in s.split(sep)])+'}'
+
+
 def make_bold(string, bold_words):
     if not bold_words:
         return string
@@ -40,7 +46,7 @@ class Template(ABC):
         self.keywords = keywords + resume.get("keywords", [])
 
     @abstractmethod
-    def new_section(self, section_name, content):
+    def new_section(self, section_name, content, summary):
         pass
 
     @abstractmethod
@@ -56,12 +62,38 @@ class Template(ABC):
         pass
 
     @abstractmethod
-    def build_resume(self):
+    def build_header(self):
         pass
 
     @abstractmethod
     def bullets_from_list(self, items):
         pass
+
+    def build_resume(self, order=['e', 'p', 'w']):
+        header = self.build_header()
+        summary = self.new_section("Summary", self.resume.get(
+            "basic_info", {"summary": ""})["summary"],summary=True)
+        sections = []
+
+        for section in order:
+            if section == 'e':
+                sections.append(self.new_section(
+                    "Education", self.load_education()))
+            elif section == 'p':
+                sections.append(self.new_section(
+                    "Projects", self.load_projects()))
+            elif section == 'w':
+                sections.append(self.new_section(
+                    "Experience", self.load_experiences()))
+
+        return f"""
+{header}
+{summary}
+{sections[0]}
+{sections[1]}
+{sections[2]}
+\\end{{document}}       
+"""
 
     def load_education(self):
         education = self.resume.get("education")
